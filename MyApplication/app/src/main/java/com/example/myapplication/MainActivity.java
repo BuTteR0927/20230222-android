de415cc21da6285ae1d3005da481eb6589addae9
Bruce <bruce802818@gmail.com>
2023-03-01 20:46:03 +0800

    0301_2 update

---
+++ MyApplication/app/src/main/java/com/example/myapplication/MainActivity.java
@@ -0,0 +1,29 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.layout_main);
+    }
+
+    public void btnIncrease_Click(View view){
+        int count;
+        TextView output = (TextView) findViewById(R.id.txvOutput);
+        count = Integer.parseInt(output.getText().toString());
+        count++;
+        output.setText(Integer.toString(count));
+    }
+
+    public void btnZero_Click(View view){
+        TextView output = (TextView) findViewById(R.id.txvOutput);
+        output.setText("0");
+    }
+}
