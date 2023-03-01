d642b403a223f52e5bec137588425a4d54b3adb4
Bruce <bruce802818@gmail.com>
2023-03-01 21:07:25 +0800

    0301_2 update

--- MyApplication/app/src/main/java/com/example/myapplication/MainActivity.java
+++ MyApplication/app/src/main/java/com/example/myapplication/MainActivity.java
@@ -22,6 +22,14 @@ public class MainActivity extends AppCompatActivity {
         output.setText(Integer.toString(count));
     }
 
+    public void btnDecrease_Click(View view){
+        int count;
+        TextView output = (TextView) findViewById(R.id.txvOutput);
+        count = Integer.parseInt(output.getText().toString());
+        count--;
+        output.setText(Integer.toString(count));
+    }
+
     public void btnZero_Click(View view){
         TextView output = (TextView) findViewById(R.id.txvOutput);
         output.setText("0");
